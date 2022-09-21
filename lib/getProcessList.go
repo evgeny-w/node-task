00a64f2e5f4ec689a4fdfc354fc49af7175c071e
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-09-21 18:12:22 +0300

    lib: add go package

---
+++ lib/getProcessList.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	ps "github.com/keybase/go-ps"
+)
+
+func main() {
+	ps, err := ps.Processes()
+
+	if err != nil {
+		fmt.Print("Error in get processes %w", err)
+		return
+	}
+
+	for pp := range ps {
+		fmt.Printf("%d %s\n", ps[pp].Pid(), ps[pp].Executable())
+	}
+}
---
+++ lib/go.mod
@@ -0,0 +1,7 @@
+module node-go-ps
+
+go 1.18
+
+require github.com/keybase/go-ps v0.0.0-20190827175125-91aafc93ba19
+
+require github.com/stretchr/testify v1.8.0 // indirect
